462e222f2e6d0648ccf29f86b148ca731240910c
Pavel Savinov <pavel.savinov@liferay.com>
2017-03-03 09:35:41 +0100

    LPS-68871 create display pages json tree to be able to display them in cards tree view

--- journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -50,6 +50,7 @@ import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProviderUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
@@ -70,6 +71,7 @@ import com.liferay.portal.kernel.search.SearchResultUtil;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
@@ -79,6 +81,7 @@ import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PrefsParamUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -88,9 +91,11 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import java.io.Serializable;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
@@ -407,6 +412,41 @@ public class JournalDisplayContext {
 		return _keywords;
 	}
 
+	public String getLayoutBreadcrumb(Layout layout) throws Exception {
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Locale locale = themeDisplay.getLocale();
+
+		List<Layout> ancestors = layout.getAncestors();
+
+		StringBundler sb = new StringBundler(4 * ancestors.size() + 5);
+
+		if (layout.isPrivateLayout()) {
+			sb.append(LanguageUtil.get(_request, "private-pages"));
+		}
+		else {
+			sb.append(LanguageUtil.get(_request, "public-pages"));
+		}
+
+		sb.append(StringPool.SPACE);
+		sb.append(StringPool.GREATER_THAN);
+		sb.append(StringPool.SPACE);
+
+		Collections.reverse(ancestors);
+
+		for (Layout ancestor : ancestors) {
+			sb.append(HtmlUtil.escape(ancestor.getName(locale)));
+			sb.append(StringPool.SPACE);
+			sb.append(StringPool.GREATER_THAN);
+			sb.append(StringPool.SPACE);
+		}
+
+		sb.append(HtmlUtil.escape(layout.getName(locale)));
+
+		return sb.toString();
+	}
+
 	public List<ManagementBarFilterItem> getManagementBarStatusFilterItems()
 		throws PortalException, PortletException {
 
@@ -531,6 +571,27 @@ public class JournalDisplayContext {
 		}
 
 		return orderColumns;
+	}
+
+	public JSONObject getPagesJSON(
+			boolean privateLayout, String selectedLayoutUuid)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		JSONArray jsonArray = _getPagesJSONArray(
+			themeDisplay.getScopeGroupId(), privateLayout, 0,
+			selectedLayoutUuid);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		jsonObject.put("children", jsonArray);
+		jsonObject.put("expanded", true);
+		jsonObject.put("icon", "home");
+		jsonObject.put("name", themeDisplay.getScopeGroupName());
+
+		return jsonObject;
 	}
 
 	public PortletURL getPortletURL() throws PortalException {
@@ -1214,6 +1275,50 @@ public class JournalDisplayContext {
 			jsonObject.put("icon", "folder");
 			jsonObject.put("id", folder.getFolderId());
 			jsonObject.put("name", folder.getName());
+
+			jsonArray.put(jsonObject);
+		}
+
+		return jsonArray;
+	}
+
+	private JSONArray _getPagesJSONArray(
+			long groupId, boolean privateLayout, long parentLayoutId,
+			String selectedLayoutUuid)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(
+			groupId, privateLayout, parentLayoutId);
+
+		for (Layout layout : layouts) {
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			jsonObject.put("icon", "page");
+			jsonObject.put("id", layout.getUuid());
+			jsonObject.put("name", layout.getName(themeDisplay.getLocale()));
+			jsonObject.put("value", getLayoutBreadcrumb(layout));
+
+			if (layout.getUuid().equals(selectedLayoutUuid)) {
+				jsonObject.put("expanded", true);
+				jsonObject.put("selected", true);
+			}
+
+			if (!layout.isContentDisplayPage()) {
+				jsonObject.put("disabled", true);
+			}
+
+			JSONArray childrenJsonArray = _getPagesJSONArray(
+				groupId, privateLayout, layout.getLayoutId(),
+				selectedLayoutUuid);
+
+			if (childrenJsonArray.length() > 0) {
+				jsonObject.put("children", childrenJsonArray);
+			}
 
 			jsonArray.put(jsonObject);
 		}
